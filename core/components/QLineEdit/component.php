bf6459dc190db8126b25fdd4ab8637a2ac9e0c95
wxmaper <wxmaper@gmail.com>
2015-08-01 21:49:30 +0700

    Add properties for QLineEdit class

--- core/components/QLineEdit/component.php
+++ core/components/QLineEdit/component.php
@@ -2,5 +2,6 @@
 
 $r['group'] = "Input Widgets";
 $r['title'] = "Line Edit";
+$r['parent'] = 'QWidget';
 $r['defobjw'] = 100;
 $r['defobjh'] = 25;
---
+++ core/components/QLineEdit/properties.php
@@ -0,0 +1,34 @@
+<?php
+
+$r = array();
+$r[] = array(
+              'title'         => tr('Text'),
+              'property'      => 'text',
+              'type'          => 'mixed'
+              );
+              
+$r[] = array(
+              'title'         => tr('Read only'),
+              'property'      => 'readOnly',
+              'type'          => 'bool',
+              'value'         => true
+              );
+              
+$r[] = array(
+              'title'         => tr('Placeholder text'),
+              'property'      => 'placeholderText',
+              'type'          => 'mixed'
+              );
+              
+$r[] = array(
+              'title'         => tr('Input mask'),
+              'property'      => 'inputMask',
+              'type'          => 'mixed'
+              );
+              
+$r[] = array(
+              'title'         => tr('Enable clear button'),
+              'property'      => 'clearButtonEnabled',
+              'type'          => 'bool',
+              'value'         => true
+              );
