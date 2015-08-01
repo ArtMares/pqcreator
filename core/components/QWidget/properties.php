ae2c988a06d447a8550f20fcf9334709c24af86c
wxmaper <wxmaper@gmail.com>
2015-08-01 14:31:15 +0700

    Add validator for properties

--- core/components/QWidget/properties.php
+++ core/components/QWidget/properties.php
@@ -2,27 +2,59 @@
 
 $r = array();
 $r[] = array(
-              'title'         => tr('Width'),
-              'property'      => 'width',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
-              'title'         => tr('Height'),
-              'property'      => 'height',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
               'title'         => tr('X'),
               'property'      => 'x',
-              'type'          => 'mixed'
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
               );
               
 $r[] = array(
               'title'         => tr('Y'),
               'property'      => 'y',
-              'type'          => 'mixed'
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Width'),
+              'property'      => 'width',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Height'),
+              'property'      => 'height',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Minimum width'),
+              'property'      => 'minimumWidth',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Maximum width'),
+              'property'      => 'maximumWidth',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Minimum height'),
+              'property'      => 'minimumHeight',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
+              );
+              
+$r[] = array(
+              'title'         => tr('Maximum height'),
+              'property'      => 'maximumHeight',
+              'type'          => 'mixed',
+              'validator'     => '[0-9]*'
               );
               
 $r[] = array(
@@ -48,30 +80,6 @@ $r[] = array(
 $r[] = array(
               'title'         => tr('Tooltip'),
               'property'      => 'toolTip',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
-              'title'         => tr('Minimum width'),
-              'property'      => 'minimumWidth',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
-              'title'         => tr('Maximum width'),
-              'property'      => 'maximumWidth',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
-              'title'         => tr('Minimum height'),
-              'property'      => 'minimumHeight',
-              'type'          => 'mixed'
-              );
-              
-$r[] = array(
-              'title'         => tr('Maximum height'),
-              'property'      => 'maximumHeight',
               'type'          => 'mixed'
               );
               
