7fcd63e9a958d4e1acf898c05f1cf1c073985d3d
wxmaper <wxmaper@gmail.com>
2015-08-01 21:49:06 +0700

    Add properties for QLabel class

--- core/components/QLabel/component.php
+++ core/components/QLabel/component.php
@@ -2,3 +2,4 @@
 
 $r['group'] = "Display Widgets";
 $r['title'] = "Label";
+$r['parent'] = 'QWidget';
---
+++ core/components/QLabel/properties.php
@@ -0,0 +1,14 @@
+<?php
+
+$r = array();
+$r[] = array(
+              'title'         => tr('Text'),
+              'property'      => 'text',
+              'type'          => 'mixed'
+              );
+              
+$r[] = array(
+              'title'         => tr('Icon'),
+              'property'      => 'icon',
+              'type'          => 'file'
+              );
