b2236719db1ab8f82be473cf425b2f1e97b2fcad
wxmaper <wxmaper@gmail.com>
2015-08-01 14:03:09 +0700

    Add properties for QCheckBox class

---
+++ core/components/QCheckBox/properties.php
@@ -0,0 +1,35 @@
+<?php
+
+$r = array();
+$r[] = array(
+              'title'         => tr('Text'),
+              'property'      => 'text',
+              'type'          => 'mixed'
+              );
+              
+$r[] = array(
+              'title'         => tr('Checkable'),
+              'property'      => 'checkable',
+              'type'          => 'bool',
+              'value'         => true
+              );
+              
+$r[] = array(
+              'title'         => tr('Checked'),
+              'property'      => 'checked',
+              'type'          => 'bool',
+              'value'         => false
+              );
+              
+$r[] = array(
+              'title'         => tr('Auto exclusive'),
+              'property'      => 'autoExclusive',
+              'type'          => 'bool',
+              'value'         => true
+              );
+              
+$r[] = array(
+              'title'         => tr('Icon'),
+              'property'      => 'icon',
+              'type'          => 'file'
+              );
